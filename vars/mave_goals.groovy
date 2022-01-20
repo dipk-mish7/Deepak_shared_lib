c009ae2631168a4815892ba085e19a38f33f1137
Deepak Mishra <deepak.mishra@mygurukulam.org>
2022-01-20 15:52:45 +0530

    Updated mave_goals.groovy

--- vars/mave_goals.groovy
+++ vars/mave_goals.groovy
@@ -1,6 +1,26 @@
-def call(){
+def compile(){
 
-echo "We are going to clean the pacakge"
-sh 'mvn clean'
+echo "We are going to compile the pacakge"
+sh 'mvn compile'
 }
+
+def stability(){
+echo "We are going to check stability the pacakge"
+sh 'mvn pmd:pmd'
+}
+
+def quality(){
+echo "We are going to check quality of the package"
+sh 'mvn checkstyle:checkstyle'
+
+}
+
+def coverage(){
+
+echo "We are going to check coverage of the package"
+sh 'mvn cobertura:cobertura'
+
+}
+
+
 
