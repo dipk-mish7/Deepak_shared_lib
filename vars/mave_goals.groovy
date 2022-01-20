705d3b18faddd15ee5bd10668f8c0ba3294c49bd
Deepak Mishra <deepak.mishra@mygurukulam.org>
2022-01-20 19:19:42 +0530

    Updated Mave

--- vars/mave_goals.groovy
+++ vars/mave_goals.groovy
@@ -7,8 +7,16 @@ sh 'mvn clean compile'
 def testing(){
 echo "We are going to test the package"
 
+// below command will check code stability.
+echo "Checking code stability"
 sh 'mvn pmd:pmd'
+
+// below command will check code coverage.
+echo "Checking code coverage"
 sh 'mvn cobertura:cobertura'
+
+// below command will check quality
+echo "Checking code quality"
 sh 'mvn checkstyle:checkstyle'
 
 }
